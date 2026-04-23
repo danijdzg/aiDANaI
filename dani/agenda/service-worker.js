5ed782b981a2b7c5dab0d946379ad1ce1b3af269
danijdzg <130553476+danijdzg@users.noreply.github.com>
2026-04-23 17:55:47 +0200

    Add files via upload

--- dani/agenda/service-worker.js
+++ dani/agenda/service-worker.js
@@ -2,7 +2,6 @@ const CACHE_NAME = 'aidanai-cache-v1';
 const urlsToCache = [
   './',
   './index.html', // Asegúrate de que el nombre del fichero es correcto
-  './aiDANaI.webp',
   'https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js',
   'https://fonts.googleapis.com/css2?family=Archivo+Narrow:wght@400;500;700&family=Roboto:wght@300;400;500;700&display=swap',
   'https://fonts.googleapis.com/icon?family=Material+Icons'
