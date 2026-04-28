c84626756ad6f722af716033788887f3332cfccb
danijdzg <130553476+danijdzg@users.noreply.github.com>
2026-04-28 12:02:09 +0200

    Add files via upload

--- dani/menu/sw.js
+++ dani/menu/sw.js
@@ -4,33 +4,45 @@ const urlsToCache = [
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png',
-  ];
+  './icon-512.png'
+];
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      const fetchPromise = fetch(event.request).then(networkResponse => {
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, networkResponse.clone());
-        });
-        return networkResponse;
-      });
-      return cachedResponse || fetchPromise;
+// 1. INSTALACIÓN: Precargar los archivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caché abierta');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 2. ACTIVACIÓN: Limpiar cachés antiguas
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Borrando caché antigua:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
 
+// 3. FETCH: Estrategia Cache-First (busca en caché, si no, va a red)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
         if (response) {
-          return response;
+          return response; // Devuelve de la caché
         }
-        return fetch(event.request);
-      }
-    )
+        return fetch(event.request); // Va a la red
+      })
   );
 });
