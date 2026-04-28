632a922c0b566593b9c00197b50392c00852e0c9
danijdzg <130553476+danijdzg@users.noreply.github.com>
2026-04-28 06:48:17 +0200

    Add files via upload

--- dani/menu/sw.js
+++ dani/menu/sw.js
@@ -7,13 +7,17 @@ const urlsToCache = [
   './icon-512.png',
   ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      const fetchPromise = fetch(event.request).then(networkResponse => {
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
+    })
   );
 });
 
